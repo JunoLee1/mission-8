dccf0573066b8dfdf9142fa4d9e23145fc9e95fb
준오 <juno@jun-oui-noteubug.local>
2025-11-05 15:47:58 +0900

    [complete]routes

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,37 +1,53 @@
 import express from "express";
 import type{Request, Response, NextFunction} from "express"
+import { ProductController } from "../controller/product.controller.js";
+import {createProductSchema, PatchProductSchema, accessListProductSchema} from"../validation/product.validation.js"
+import {validateBody, validateQuery} from"../middleWare/validateMiddle.js"
+import passport from "passport";
 
 
+const productController=  new ProductController
 const router = express.Router();
 
 
 // API : 상품 데이터 리스트 조회
-router.get("/",async(req:Request, res: Response, next:NextFunction) =>{
-    //productService.AccessListProduct(req, res, next)
+router.get("/",
+    validateQuery(accessListProductSchema),
+    async(req:Request, res: Response, next:NextFunction) =>{
+    productController.accessListProduct(req, res, next)
 })
 
 
 // API : 상품 데이터 조회
-router.get("/:id",async(req:Request, res: Response, next:NextFunction) =>{
-    //productService.AccessProduct(req, res, next)
+router.get("/:id",
+    async(req:Request, res: Response, next:NextFunction) =>{
+    productController.accessProduct(req, res, next)
 })
 
 
 // API : 상품 데이터 생성
-router.post("/",async(req:Request, res: Response, next:NextFunction) =>{
-    //productService.createProduct(req, res, next)
+router.post("/",
+    passport.authenticate("local",{session:false}),
+    validateBody(createProductSchema),
+    async(req:Request, res: Response, next:NextFunction) =>{
+    productController.createProduct(req, res, next)
 })
 
 
 // API : 상품 정보 수정
-router.patch("/:id",async(req:Request, res: Response, next:NextFunction) =>{
-    //productService.modifyProduct(req, res, next)
+router.patch("/:id",
+    passport.authenticate("local",{session:false}),
+    validateBody(PatchProductSchema),
+    async(req:Request, res: Response, next:NextFunction) =>{
+    productController.modifyProduct(req, res, next)
 })
 
 
 // API : 상품 삭제
-router.delete("/:id",async(req:Request, res: Response, next:NextFunction) =>{
-    //productService.deleteProduct(req, res, next)
+router.delete("/:id",
+    passport.authenticate("local",{session:false}),
+    async(req:Request, res: Response, next:NextFunction) =>{
+    productController.deleteProduct(req, res, next)
 })
 
 export default router;// router 만 내보내기
