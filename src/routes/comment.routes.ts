892ccfe0d02df5ae064c561d5f6b86c4bd6bc083
준오 <juno@jun-oui-noteubug.local>
2025-11-06 16:12:44 +0900

    routes

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,31 +1,49 @@
 import type { Request, Response, NextFunction } from "express";
 import express from "express"
+import {validateParam,validateBody, validateQuery} from"../middleWare/validateMiddle.js"
+import{ commentParamSchema, commentPatchSchema, commentBodySchema, commentQuerySchema } from "../validation/comment.validation.js"
+import passport from "passport";
+
 
 const router = express.Router()
 
 
 // 댓글 리스트 및 댓글 조회 
-router.get("/",async(req:Request, res:Response, next:NextFunction)=>{
+router.get("/",
+    validateQuery(commentQuerySchema),
+    async(req:Request, res:Response, next:NextFunction)=>{
     // commentController.accessCommentList(req, res, next)
 })
 
 
-router.get("/:id",async(req:Request, res:Response, next:NextFunction)=>{
+router.get("/:id",
+    validateParam(commentParamSchema),
+    async(req:Request, res:Response, next:NextFunction)=>{
     // commentController.accessComment(req, res, next)
 })
 
 // 회원만 댓글 생성/ 수정/ 삭제 가능
-router.post("/",async(req:Request, res:Response, next:NextFunction)=>{
+router.post("/",
+    passport.authenticate("local",{session:false}),
+    validateBody(commentBodySchema),
+    async(req:Request, res:Response, next:NextFunction)=>{
     // commentController.createComment(req, res, next)
 })
 
 
-router.patch("/:id",async(req:Request, res:Response, next:NextFunction)=>{
+router.patch("/:id",
+    passport.authenticate("local",{session:false}),
+    validateParam(commentParamSchema),
+    validateBody(commentPatchSchema),
+    async(req:Request, res:Response, next:NextFunction)=>{
     // commentController.modifyComment(req, res, next)
 })
 
 
-router.delete("/:id",async(req:Request, res:Response, next:NextFunction)=>{
+router.delete("/:id",
+    passport.authenticate("local",{session:false}),
+     validateParam(commentParamSchema),
+    async(req:Request, res:Response, next:NextFunction)=>{
     // commentController.deleteComment(req, res, next)
 })
 
